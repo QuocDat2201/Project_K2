45ade4da446de93c5f586794d236337122064f45
QuocDat2201 <hoquocdat220104@gmail.com>
2023-10-28 13:50:24 +0700

    them models

--- Project_K2/src/models/Category_model.java
+++ Project_K2/src/models/Category_model.java
@@ -1,5 +1,23 @@
 package models;
 
+import java.sql.PreparedStatement;
+
+import entites.Category;
+
 public class Category_model {
-	
+	public boolean Create(Category category) {
+		boolean result = true;
+		try {
+			PreparedStatement preparedStatement = ConnectDB.connection()
+					.prepareStatement("insert into category(CategoryName) values(?)");
+			preparedStatement.setString(1, category.getCategoryName());
+			result = preparedStatement.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = false;
+		} finally {
+			ConnectDB.disconnect();
+		}
+		return result;
+	}
 }
--- Project_K2/src/models/UsersModel.java
+++ Project_K2/src/models/UsersModel.java
@@ -8,32 +8,54 @@ import java.util.List;
 import entites.Users;
 
 public class UsersModel {
-	 public List<Users> findAll(){
-		 List<Users> users = new ArrayList<Users>();
-		 try {
-			 PreparedStatement preparedStatement = ConnectDB.connection().prepareStatement("select * from users");
-			 ResultSet resultSet = preparedStatement.executeQuery();
-				while(resultSet.next()) {
-					Users user = new Users() ;
-					user.setUserID(resultSet.getInt("UserID"));
-					user.setUsername(resultSet.getString("Username"));
-					user.setPassword(resultSet.getString("Password"));
-					user.setRoleID(resultSet.getInt("RoleID"));
-					users.add(user);
-				}
+	public List<Users> findAll() {
+		List<Users> users = new ArrayList<Users>();
+		try {
+			PreparedStatement preparedStatement = ConnectDB.connection().prepareStatement("select * from users");
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				Users user = new Users();
+				user.setUserID(resultSet.getInt("UserID"));
+				user.setUsername(resultSet.getString("Username"));
+				user.setPassword(resultSet.getString("Password"));
+				user.setRoleID(resultSet.getInt("RoleID"));
+				users.add(user);
+			}
 		} catch (Exception e) {
-			users = null ; 
-		}finally {
+			users = null;
+		} finally {
 			ConnectDB.disconnect();
-		}return users ; 
-	 }
-	 
-	 public Users login(String username , String password) {
-		 UsersModel usersModel = new UsersModel() ; 
-		 for(Users user : usersModel.findAll()) {
-			 if(user.getUsername().equalsIgnoreCase(username) && user.getPassword().equalsIgnoreCase(password)) {
-				 return user ; 
-			 }
-		 }return null ; 
-	 }
+		}
+		return users;
+	}
+
+	public Users login(String username, String password) {
+		UsersModel usersModel = new UsersModel();
+		for (Users user : usersModel.findAll()) {
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equalsIgnoreCase(password)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public boolean register(Users users) {
+		boolean result = true;
+		try {
+			PreparedStatement preparedStatement = ConnectDB.connection()
+					.prepareStatement("insert into users(Username, Password, RoleID) values(?,?,?)");
+			preparedStatement.setString(1, users.getUsername());
+			preparedStatement.setString(2, users.getPassword());
+			preparedStatement.setInt(3, users.getRoleID());
+
+			result = preparedStatement.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = false;
+		} finally {
+			ConnectDB.disconnect();
+		}
+		
+		return result;
+	}
 }
