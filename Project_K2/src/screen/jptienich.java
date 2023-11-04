0ea983e302c989179e7382fd78f4b739a247c026
tn018 <tn018@LAPTOP-LA5V5VPQ>
2023-11-04 21:53:35 +0700

    d

--- Project_K2/src/screen/jptienich.java
+++ Project_K2/src/screen/jptienich.java
@@ -1,6 +1,7 @@
 package screen;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class jptienich extends JPanel {
 
@@ -8,6 +9,10 @@ public class jptienich extends JPanel {
 	 * Create the panel.
 	 */
 	public jptienich() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.CENTER);
 
 	}
 
