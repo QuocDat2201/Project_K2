c3cf602c428d0d80ea6dc857c7ba1e9e8ddcd323
tn018 <tn018@LAPTOP-LA5V5VPQ>
2023-11-04 23:37:47 +0700

    ad

--- Project_K2/src/screen/JPcustomer.java
+++ Project_K2/src/screen/JPcustomer.java
@@ -12,6 +12,11 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import entites.Customer;
+import models.CustomerModel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -25,28 +30,28 @@ public class JPcustomer extends JPanel {
 	 */
 	public JPcustomer() {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.CENTER);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.NORTH);
-		
+
 		textField = new JTextField();
 		panel_1.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("New label");
 		panel_1.add(lblNewLabel);
-		
+
 		textField_1 = new JTextField();
 		panel_1.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		panel_1.add(lblNewLabel_1);
-		
+
 		JButton btnNewButton = new JButton("Add Customer");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -54,29 +59,51 @@ public class JPcustomer extends JPanel {
 			}
 		});
 		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
-		btnNewButton.setIcon(new ImageIcon(JPcustomer.class.getResource("/Icon/403022_business man_male_user_avatar_profile_icon.png")));
+		btnNewButton.setIcon(new ImageIcon(
+				JPcustomer.class.getResource("/Icon/403022_business man_male_user_avatar_profile_icon.png")));
 		panel_1.add(btnNewButton);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBackground(new Color(255, 0, 0));
 		panel.add(scrollPane, BorderLayout.CENTER);
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		add(menuBar, BorderLayout.NORTH);
-		
+
 		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
 		menuBar.add(mntmNewMenuItem);
-		
+
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
 		menuBar.add(mntmNewMenuItem_1);
-		
+
 		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
 		menuBar.add(mntmNewMenuItem_2);
-
+		iniJFrame();
 	}
+
 	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+	}
+
+	private void iniJFrame() {
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Name");
+		model.addColumn("Phone");
+		model.addColumn("Point");
+		model.addColumn("Rank");
+		CustomerModel customerModel = new CustomerModel();try {
+			for (Customer customer : customerModel.findAll()) {
+				model.addRow(new Object[] { customer.getNameString(), customer.getPhoneString(), customer.getPoint(),
+						customer.getRank() });
+
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		table.setModel(model);
+
 	}
 }
